1656742ed2256b5f1901b1dfe112df8d15ba0be9
Gunnar Normand <gunnar@gunnar.tech>
2020-01-19 00:47:35 -0500

    add customizer setting and control for toggling usage of the_content() or the_excerpt() in archives.

--- wp-content/themes/wprig-nona/inc/Archive_Content/Component.php
+++ wp-content/themes/wprig-nona/inc/Archive_Content/Component.php
@@ -8,6 +8,9 @@
 namespace WP_Rig\WP_Rig\Archive_Content;
 
 use WP_Rig\WP_Rig\Component_Interface;
+use WP_Customize_Manager;
+use function add_action;
+use function __;
 
 /**
  * Class for allowing administrators to decide whether to display content or excerpt in archives.
@@ -27,6 +30,32 @@ class Component implements Component_Interface {
 	 * Adds the action and filter hooks to integrate with WordPress.
 	 */
 	public function initialize() {
-		// TODO: Add actions and filters here.
+		add_action( 'customize_register', array( $this, 'action_register_customizer_control' ) );
+	}
+
+	/**
+	 * Adds a Customizer setting and control for toggling usage of `the_content()` or `the_excerpt()` in archives.
+	 *
+	 * @param WP_Customize_Manager $wp_customize Customizer manager instance.
+	 */
+	public function action_register_customizer_control( WP_Customize_Manager $wp_customize ) {
+		// Register the Customizer setting and ensure its value is a boolean.
+		$wp_customize->add_setting(
+			'archives_use_excerpt',
+			array(
+				'default'           => false,
+				'sanitize_callback' => 'rest_sanitize_boolean',
+			)
+		);
+
+		// Register the Customizer control for the setting, using a checkbox.
+		$wp_customize->add_control(
+			'archives_use_excerpt',
+			array(
+				'label'   => __( 'Use excerpts in archive views?', 'wp-rig' ),
+				'section' => 'theme_options',
+				'type'    => 'checkbox',
+			)
+		);
 	}
 }
