34f88b63c532c34a8a26b20e22241814ea1405ae
Gunnar Normand <gunnar@gunnar.tech>
2021-04-16 22:46:22 -0400

    remove accent from entry footer

--- wp-content/themes/wprig-nona/template-parts/content/entry_footer.php
+++ wp-content/themes/wprig-nona/template-parts/content/entry_footer.php
@@ -12,16 +12,4 @@ namespace WP_Rig\WP_Rig;
 	<?php get_template_part( 'template-parts/content/entry_taxonomies', get_post_type() ); ?>
 
 	<?php get_template_part( 'template-parts/content/entry_actions', get_post_type() ); ?>
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 766.38 349.07">
-		<defs>
-			<style>
-				.eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae {
-					fill: #038e3e;
-				}
-			</style>
-		</defs>
-		<g id="fe203fe9-b447-42e3-abbc-84d00a4a38c0" data-name="Layer 2">
-			<g id="b7080531-dc36-4626-bb8d-a656577f4838" data-name="Layer 1"><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M615.35,284.06s-8.21-19.34,11-36.76C626.34,247.3,638.62,270.75,615.35,284.06Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M727.12,319.91s7.32-24.86,39.26-26.42C766.38,293.49,759.72,325.46,727.12,319.91Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M620.22,325.2s25.64-5.11,42,22.68C662.24,347.88,630.55,356.92,620.22,325.2Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M186.31,116.9s-15.77-7.74-13.15-29.25C173.16,87.65,193.75,95.76,186.31,116.9Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M126.08,188.2s-5.11-13,8-24.31C134.1,163.89,141.86,179.77,126.08,188.2Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M0,69S12.86,50.12,40.12,57.43C40.12,57.43,25.89,82.5,0,69Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M537.72,148.07s-21.45-7.3-21.66-35.26C516.06,112.81,543.75,119.79,537.72,148.07Z"/><path class="eef6c848-8f3f-4bc6-a1aa-9ac8d31e91ae" d="M520,24.81S526.4,2.21,557.16,0C557.16,0,551.59,29,520,24.81Z"/></g>
-		</g>
-	</svg>
 </footer><!-- .entry-footer -->
