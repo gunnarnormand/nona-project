2e01695d837312c10b871de1bb86570f832dcbe2
Gunnar Normand <gunnar@gunnar.tech>
2021-04-16 22:47:57 -0400

    remove AMP header animation

--- wp-content/themes/wprig-nona/header.php
+++ wp-content/themes/wprig-nona/header.php
@@ -37,60 +37,8 @@ namespace WP_Rig\WP_Rig;
 
 	<header id="masthead" class="site-header">
 
-		<?php
-		if ( wp_rig()->is_amp() ) {
-			?>
-			<amp-animation id="headerNotScrolled" layout="nodisplay">
-				<script type="application/json">
-				{
-					"duration": "1000ms",
-					"easing": "cubic-bezier(0.165, 0.84, 0.44, 1)",
-					"fill": "both",
-					"iterations": "1",
-					"direction": "alternate",
-					"animations": [{
-						"selector": "#masthead .site-branding",
-						"keyframes": "keyframesNotScrolled"
-					}
-					]
-				}
-				</script>
-			</amp-animation>
-
-			<amp-animation id="headerScrolled" layout="nodisplay">
-				<script type="application/json">
-				{
-					"duration": "1000ms",
-					"easing": "cubic-bezier(0.165, 0.84, 0.44, 1)",
-					"fill": "both",
-					"iterations": "1",
-					"direction": "alternate",
-					"animations": [{
-						"selector": "#masthead .site-branding",
-						"keyframes": "keyframesScrolled"
-					}
-					]
-				}
-				</script>
-			</amp-animation>
-			<?php
-		}
-		?>
-
-		<?php get_template_part( 'template-parts/header/custom_header' ); ?>
 		<?php get_template_part( 'template-parts/header/branding' ); ?>
 		<?php get_template_part( 'template-parts/header/custom_logo' ); ?>
 		<?php get_template_part( 'template-parts/header/navigation' ); ?>
 
 	</header><!-- #masthead -->
-
-	<div class="scroll-marker">
-		<?php
-		if ( wp_rig()->is_amp() ) {
-			?>
-			<amp-position-observer intersection-ratios="0" on="enter:headerScrolled.start; exit:headerNotScrolled.start" layout="nodisplay">
-			</amp-position-observer>
-			<?php
-		}
-		?>
-	</div>
